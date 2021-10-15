06a1547ad635ae843ac96615a3cf87ddce8f56a7
nelsontr <trindade.nelson99@gmail.com>
2021-10-15 09:46:33 +0100

    adding tests | updating script

--- Project1/runT.sh
+++ Project1/runT.sh
@@ -1,28 +1,72 @@
 #!/bin/bash
 
-echo ">> Compiling (sources/):"
-for i in sources/*.txt; do
-	printf "$i | "
-    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
-done
-printf "\n\n"
+i=0
+sourceTXT=()
 
-echo ">> Compiling (tests/):"
-for i in tests/*.txt; do
-	printf "$i | "
-    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
-done
-printf "\n\n"
+if [ $# -eq 0 ] 
+then
+	sourceTXT=("leap" "skip" "mm2mmm" "copy" "d2dddd" "d2dd" "date2year" "R2A")
+else
+	for arg; do
+		sourceTXT[i]=$arg
+		i=$((i+1))
+	done
+fi
 
-# TODO
-echo ">> Creating image (images/):"
-for i in compiled/*.fst; do
-	printf "$(basename $i '.fst').pdf | "
-    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
-done
-printf "\n\n"
+if [ $# -eq 0 ] 
+then
+	echo ">> Compiling (sources/):"
+	for i in sources/*.txt; do
+		printf "$i | "
+	    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+	done
+	printf "\n\n"
 
-sourceTXT=("leap" "skip" "mm2mmm" "copy" "d2dddd" "d2dd" "date2year" "R2A")
+	echo ">> Compiling (tests/):"
+	for i in tests/*.txt; do
+		printf "$i | "
+	    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+	done
+	printf "\n\n"
+
+	# TODO
+	echo ">> Creating image (images/):"
+	for i in compiled/*.fst; do
+		printf "$(basename $i '.fst').pdf | "
+	    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
+	done
+	printf "\n\n"
+else
+	echo ">> Compiling (sources/):"
+	for ii in ${sourceTXT[*]}; do
+		for i in sources/$ii.txt; do
+			printf "$i | "
+			fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+		done
+	done
+	printf "\n\n"
+
+	echo ">> Compiling (tests/):"
+	for ii in ${sourceTXT[*]}; do
+		for i in tests/$ii[0-9]*.txt; do
+			printf "$i | "
+		    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+		done
+	done
+	printf "\n\n"
+
+	# TODO
+	echo ">> Creating image (images/):"
+	for ii in ${sourceTXT[*]}; do
+		for i in compiled/$ii[0-9]*.fst; do
+			printf "$(basename $i '.fst').pdf | "
+		    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
+		done
+	done
+	printf "\n\n"
+fi
+
+
 
 for i in ${sourceTXT[*]}; do
 	for ii in tests/$i[0-9]*.txt; do
