ce6e746ef8015775f044ac02f259a6fb97cca5fe
nelsontr <trindade.nelson99@gmail.com>
2021-10-15 11:29:31 +0100

    updating runT.sh

--- Project1/runT.sh
+++ Project1/runT.sh
@@ -3,28 +3,34 @@
 i=0
 sourceTXT=()
 
-if [ $# -eq 0 ] 
+if [ $# -ne 0 ] 
 then
+	if [ $1 == "-h" ] 
+	then
+		echo "('leap' 'skip' 'mm2mmm' 'copy' 'd2dddd' 'd2dd' 'date2year' 'R2A')"
+		exit 0
+	else
+		for arg; do
+			sourceTXT[i]=$arg
+			i=$((i+1))
+		done
+	fi
+else 
 	sourceTXT=("leap" "skip" "mm2mmm" "copy" "d2dddd" "d2dd" "date2year" "R2A")
-else
-	for arg; do
-		sourceTXT[i]=$arg
-		i=$((i+1))
-	done
 fi
 
 if [ $# -eq 0 ] 
 then
 	echo ">> Compiling (sources/):"
 	for i in sources/*.txt; do
-		printf "$i | "
+		printf "$(basename $i) | "
 	    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 	done
 	printf "\n\n"
 
 	echo ">> Compiling (tests/):"
 	for i in tests/*.txt; do
-		printf "$i | "
+		printf "$(basename $i) | "
 	    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
 	done
 	printf "\n\n"
@@ -40,8 +46,8 @@ else
 	echo ">> Compiling (sources/):"
 	for ii in ${sourceTXT[*]}; do
 		for i in sources/$ii.txt; do
-			printf "$i | "
-			fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+			printf "$(basename $i) | "
+			fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $ii ".txt").fst
 		done
 	done
 	printf "\n\n"
@@ -49,23 +55,21 @@ else
 	echo ">> Compiling (tests/):"
 	for ii in ${sourceTXT[*]}; do
 		for i in tests/$ii[0-9]*.txt; do
-			printf "$i | "
-		    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $i ".txt").fst
+			printf "$(basename $i) | "
+		    fstcompile --isymbols=syms.txt --osymbols=syms.txt $i | fstarcsort > compiled/$(basename $ii ".txt").fst
 		done
 	done
 	printf "\n\n"
 
-	# TODO
 	echo ">> Creating image (images/):"
 	for ii in ${sourceTXT[*]}; do
 		for i in compiled/$ii[0-9]*.fst; do
 			printf "$(basename $i '.fst').pdf | "
-		    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
+		    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $ii '.fst').pdf
 		done
 	done
 	printf "\n\n"
 fi
-
 
 
 for i in ${sourceTXT[*]}; do
