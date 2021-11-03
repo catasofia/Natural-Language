1b16479ce6cafa4feae3c2400bbca91324eaed03
nelsontr <trindade.nelson99@gmail.com>
2021-11-03 17:19:34 +0000

    88.4%

--- Project2/qc.py
+++ Project2/qc.py
@@ -1,31 +1,23 @@
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize
 import sys
 
-from nltk.stem.porter import *
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, TfidfVectorizer
-from sklearn import svm
 from sklearn.svm import SVC
 
 import nltk
-nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 
   
 def parser(fileName):
-  sentences=[]
-  data=[]
-  labels =[]
+  data, labels, sentences=[],[],[]
 
   with open(fileName,'r',encoding='UTF8') as file:
-    while True:
-      line=file.readline().replace("\n","").replace("\"","").replace(",","").split("\t")
-      if (line==[""]):
-        break
+    for line in file:
+      line=line.replace("\n","").replace("\"","").replace(",","").split("\t")
       labels.append(line[0])
       sentences.append(line[1:])
   return labels, sentences
@@ -38,22 +30,19 @@ def processing(sentences):
   for i in sentences:
     tokens = word_tokenize(str(i))
 
-    minusculo = [word.lower() for word in tokens]
+    #lowCase = [word.lower() for word in tokens]
 
-    stem = [stemmer.stem(word) for word in minusculo]
+    #stem = [stemmer.stem(word) for word in lowCase]
 
-    lemma = [lemmatizer.lemmatize(word) for word in filtered_words]
+    #lemma = [lemmatizer.lemmatize(word) for word in stem]
 
-    questions = ' '.join(lemma)
-
-    questionsList.append(questions)
+    questionsList.append(' '.join(tokens))
   
   return questionsList
 
 # process input files
-
-train_file = sys.argv[4]
 test_file = sys.argv[2]
+train_file = sys.argv[4]
 
 labels, questions = parser(train_file)
 trainProcessed = processing(questions)
@@ -61,22 +50,21 @@ trainProcessed = processing(questions)
 labelsTest, questionsTest = parser(test_file)
 testProcessed = processing(questionsTest)
 
-#support vector machines
 
+#support vector machines
 countVectorizer = CountVectorizer()
+tfIdfTransformer = TfidfTransformer()
+classifier = SVC()
 
 
 #fit transform in training set and transform for test set
 trainVector = countVectorizer.fit_transform(trainProcessed)
 testVector  = countVectorizer.transform(testProcessed)
 
-tfIdfTransformer = TfidfTransformer()
 train_tfidf = tfIdfTransformer.fit_transform(trainVector)
 test_tfidf  = tfIdfTransformer.transform(testVector)
 
-classifier = svm.SVC()
 classifier.fit(train_tfidf, labels)
-
 labels_predict = classifier.predict(test_tfidf)
 
 
